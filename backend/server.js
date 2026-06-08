c472c1695b8327622e45f6b850717b665a933d30
Nikil <nikilreddy3406@gmail.com>
2026-06-08 20:53:02 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-vercel-app.vercel.app"
+    "https://gully-news.vercel.app/"
   ],
   credentials: true
 }));
